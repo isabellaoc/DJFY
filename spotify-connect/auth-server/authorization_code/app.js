3402c2353ddd47ac6df977e024eb4f540ca1ce35
Afnaan Karimi <24482619+afnaank@users.noreply.github.com>
2019-04-15 03:01:04 -0400

    Updated redirect uri
    Redirects to create room page

--- spotify-connect/auth-server/authorization_code/app.js
+++ spotify-connect/auth-server/authorization_code/app.js
@@ -15,7 +15,7 @@ var cookieParser = require('cookie-parser');
 
 var client_id = 'ed2d4bbb071e479486b956e0749346f8'; // Your client id
 var client_secret = '1261a30a6827432e8ddba467b29d17ef'; // Your secret
-var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+var redirect_uri = 'http://www.reachip.com/createroom.html'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
@@ -104,7 +104,7 @@ app.get('/callback', function(req, res) {
         });
 
         // we can also pass the token to the browser to make requests from there
-        res.redirect('http://localhost:3000/#' +
+        res.redirect('http://www.reachip.com/createroom.html' +
           querystring.stringify({
             access_token: access_token,
             refresh_token: refresh_token
