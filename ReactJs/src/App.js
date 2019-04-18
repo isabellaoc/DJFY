cfdd98ae2825afdc8324d2beff91e6adb2b23119
BridgetTrenchard <44105291+BridgetTrenchard@users.noreply.github.com>
2019-04-18 11:13:59 -0400

    Add files via upload

--- ReactJs/src/App.js
+++ ReactJs/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 /*import {render} from "render.dom";*/
-import {
+/*import {
   BrowserRouter as Router,
   Route,
   //Redirect,
   //withRouter,
   Link
-} from "react-router-dom";
+} from "react-router-dom";*/
 import './App.css';
 
 
@@ -36,78 +36,49 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
         <div className="App">
-        <h1>DJfy</h1>
+          <h1>DJfy</h1>
+          <Connect> </Connect>
+          <Room> </Room>
 
-              <Route path = "/home" render ={
-                ()=> {
-                  return(
-                    <div>
-                      <button><Link to = "/createroom"> Create Room </Link></button>
-                        <br/> <br/>
-                      <button><Link to = "/joinroom"> Join Room </Link></button>
-                    </div>
-                    );
-                }
-              }/>
-
-
-              <Route path = "/createroom" render ={
-                ()=> {
-                  var rand = (Math.random() * (9999999999));
-                    return (
-                      <div className="CreateRoom">
-                        <h2>Create Room</h2>
-              
-                          Enter Playlist Name: 
-                          <input type = "text" onChange = {this.changePlaylistName} value = {this.state.playlistName}/> 
-                          
-                          <br/> <br/>
-                              <button onClick = {this.changeRoomCode.bind(this, {rand})}><Link to = "/Room/{rand}"> Create Room </Link></button>
-                          <br/> <br/>
-                      </div>  
-                    );
-                }
-              }/>
-
-              <Route path = "/joinroom" render ={
-                ()=> {
-                  return (
-                    <div className="JoinRoom">
-                      <h2>Join Room</h2>
-                        Enter Room Code:  
-                          <input type = "text" onChange = {this.changeRoomCodeLogin} value = {this.state.roomCodeLogin}/>
-
-                          <br/> <br/>
-                              <button>Join Room</button>
-                          <br/> <br/>
-                    </div>
-                  );
-                }
-              }/>
-
-            <Route path = "/Room" render ={
-                ()=> {
-                  return (
-                    <div className="Room">
-                      <h3>{this.state.playlistName}</h3>
-  
-                      <ul>
-                          Search: <input type = "text"/>
-                          <br/> <br/>
-
-                          <li><button style = {{width: 25, height: 25, borderWidth: 3, fontSize: 15, backgroundColor: '#FF0000'}}>-</button> Song 1</li>
-                          <li><button style = {{width: 25, height: 25, borderWidth: 3, fontSize: 15, backgroundColor: '#FF0000'}}>-</button> Song 2</li>
-                          <li><button style = {{width: 25, height: 25, borderWidth: 3, fontSize: 15, backgroundColor: '#FF0000'}}>-</button> Song 3</li>
-                      </ul>
-                    </div>
-                  );
-                }
-              }/>
-        </div>
-      </Router>
+    
+        </div>    
     );
   }
 }
+
+class Connect extends Component {
+
+  render() {
+    return (
+        <div className="Connect">
+          <h3>Connect to Spotify <button>Connect</button> Join Room <button> Join </button></h3>
+        </div>    
+    );
+  }
+}
+
+
+
+class Room extends Component {
+
+  render() {
+    return (
+      <div className="Room">
+      <h2>Playlist</h2>
+
+      <ul>
+          Search: <input type = "text"/>
+          <br/> <br/>
+
+          <li><button style = {{width: 25, height: 25, borderWidth: 3, fontSize: 15, backgroundColor: '#FF0000'}}>-</button> Song 1</li>
+          <li><button style = {{width: 25, height: 25, borderWidth: 3, fontSize: 15, backgroundColor: '#FF0000'}}>-</button> Song 2</li>
+          <li><button style = {{width: 25, height: 25, borderWidth: 3, fontSize: 15, backgroundColor: '#FF0000'}}>-</button> Song 3</li>
+      </ul>
+    </div>    
+    );
+  }
+}
+
+
 export default App;
