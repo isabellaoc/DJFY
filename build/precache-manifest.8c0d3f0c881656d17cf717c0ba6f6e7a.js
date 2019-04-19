1f7dca266c0c14c7691df4450032b2b62ae5be75
Afnaan Karimi <24482619+afnaank@users.noreply.github.com>
2019-04-18 20:16:51 -0400

    Add build

---
+++ build/precache-manifest.8c0d3f0c881656d17cf717c0ba6f6e7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c481a4a563765418f319",
+    "url": "/static/css/main.35d5926e.chunk.css"
+  },
+  {
+    "revision": "c481a4a563765418f319",
+    "url": "/static/js/main.cc036afd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f9f1c06c9d9e12a3f256",
+    "url": "/static/js/2.8459dc34.chunk.js"
+  },
+  {
+    "revision": "b43f8100a9ec6538dd4dfb938cd94c7a",
+    "url": "/index.html"
+  }
+];
