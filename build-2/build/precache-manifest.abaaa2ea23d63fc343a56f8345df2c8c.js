ef3e9d9b2a2c00ef7b3aba72965a04d2090ea1bd
Afnaan Karimi <24482619+afnaank@users.noreply.github.com>
2019-04-19 15:53:50 -0400

    react-client-app build 2

---
+++ build-2/build/precache-manifest.abaaa2ea23d63fc343a56f8345df2c8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fc4a6ce5cbcbae098668",
+    "url": "/static/js/main.5ff8a09e.chunk.js"
+  },
+  {
+    "revision": "1496a45a429c139e3584",
+    "url": "/static/js/2.be6af7b2.chunk.js"
+  },
+  {
+    "revision": "fc4a6ce5cbcbae098668",
+    "url": "/static/css/main.35d5926e.chunk.css"
+  },
+  {
+    "revision": "15e9f7f3eedb67941f25e659c0e0ad0c",
+    "url": "/index.html"
+  }
+];
