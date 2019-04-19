1a795e755207c58fa3bdccc01d6e7ae4421c6eab
BridgetTrenchard <44105291+BridgetTrenchard@users.noreply.github.com>
2019-04-18 20:07:36 -0400

    Add files via upload

--- Updated-React/react-client-app/src/App.js
+++ Updated-React/react-client-app/src/App.js
@@ -4,6 +4,11 @@ import './css/bootstrap.css';
 import SpotifyWebApi from 'spotify-web-api-js';
 const spotifyApi = new SpotifyWebApi();
 
+var tracks = ["Song 1", "Song 2", "Song 3", "Song 4", "Song 5"];
+var listItems = tracks.map((tracks) =>
+  <li><button style = {{width: 25, height: 25, borderWidth: 3, fontSize: 15, backgroundColor: '#FF0000'}}>-</button>{tracks}</li>
+);
+
 class App extends Component {
 
     // Get access token to be able to fetch data from the Spotify API
@@ -16,10 +21,31 @@ class App extends Component {
       }
       this.state = {
           loggedIn: token ? true : false,
-          spotifyAccount: {accountName: 'Not Logged In', accountPic: ''}
+          spotifyAccount: {accountName: 'Not Logged In', accountPic: ''},
+          
+          playlistName:'Playlist Name',
+          search: '',
+          };
+          
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
       }
-    }
+    
 
+    handleChange(event) {
+        this.setState({search: event.target.value});
+      }
+    
+    handleSearch(event) {
+        alert(this.search.value);
+        event.preventDefault();
+    }
+    
+    changePlaylistName = (event) =>  {
+        this.setState({
+        playlistName: event.target.value 
+          })
+    }
     // Using code from authorization_code/public/index.html
     // (https://github.com/spotify/web-api-auth-examples)
     // Extracts token params from hash string of the URL into an object with key-value pairs.
@@ -63,7 +89,7 @@ class App extends Component {
                 Spotify Account: {this.state.spotifyAccount.accountName}
             </div>
             <div>
-                <img src={this.state.spotifyAccount.accountPic} style={{ height: 100 }}/>
+                <img  src={this.state.spotifyAccount.accountPic} style={{ height: 100 }}/>
                 <br /> <br />
             </div>
 
@@ -86,30 +112,28 @@ class App extends Component {
                 <button class="btn btn-lg btn-secondary" onClick={() => this.joinRoom()}>Join</button>
             </p>
 
-            <Room> </Room>
+            <h2>{this.state.playlistName}</h2>
+            <h3>{this.state.search}</h3>
+
+            <ul align = "left">
+            <h2>{this.state.playlistName}</h2> 
+            <form onSubmit={this.handleSubmit}>
+              <label>
+                Search:
+                <textarea  type = 'text' value={this.state.search} onChange={this.handleChange} />
+              </label>
+              <input type="submit" value="Search" />
+            </form>
+              
+                  <br/> <br/>
+                  {listItems}
+            </ul> 
 
           </div>
         );
     }
 }
 
-class Room extends Component {
-    render() {
-      return (
-        <div className="Room">
-        <h2>Playlist</h2>
 
-        <ul>
-            Search: <input type = "text"/>
-            <br/> <br/>
-
-            <li><button style = {{width: 25, height: 25, borderWidth: 3, fontSize: 15, backgroundColor: '#FF0000'}}>-</button> Song 1</li>
-            <li><button style = {{width: 25, height: 25, borderWidth: 3, fontSize: 15, backgroundColor: '#FF0000'}}>-</button> Song 2</li>
-            <li><button style = {{width: 25, height: 25, borderWidth: 3, fontSize: 15, backgroundColor: '#FF0000'}}>-</button> Song 3</li>
-        </ul>
-      </div>
-      );
-    }
-}
 
 export default App;
