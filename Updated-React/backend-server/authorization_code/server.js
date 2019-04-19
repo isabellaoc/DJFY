b48246c1d5ca58d9baa9070c080c5a305eacaa17
Afnaan Karimi <24482619+afnaank@users.noreply.github.com>
2019-04-18 21:00:42 -0400

    Add playlist-modify-private scope

--- Updated-React/backend-server/authorization_code/server.js
+++ Updated-React/backend-server/authorization_code/server.js
@@ -46,10 +46,10 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email';
+  var scope = 'user-read-private user-read-email playlist-modify-private';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
-      response_type: 'code',
+      response_type: 'code',user-read
       client_id: client_id,
       scope: scope,
       redirect_uri: redirect_uri,
